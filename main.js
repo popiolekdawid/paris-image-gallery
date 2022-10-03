e4bcf1c7d525cc9006529d0d83810cd1bdb3c233
popiolekdawid <112573508+popiolekdawid@users.noreply.github.com>
2022-10-03 18:33:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,6 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 const fileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
-/* Declaring the alternative text for each image file */
-
 /* Looping through images */
 for(const image of fileNames){
     const newImage = document.createElement('img');
@@ -18,6 +16,7 @@ for(const image of fileNames){
     newImage.addEventListener("click", (event) => {displayedImage.src = newImage.src;
                                                    displayedImage.alt = newImage.alt});
 }
+
 /* Wiring up the Darken/Lighten button */
 function changeColor() {
     if(btn.getAttribute("class") == "dark") {
